acf5bc68a951c30ff15ddb12306c134d25b8ccd0
Sara Connelly <sara.k.connelly@gmail.com>
2023-06-08 13:12:29 -0600

    Add sara's methods to variables and arrays

--- builtInMethods.js
+++ builtInMethods.js
@@ -8,13 +8,17 @@
 // The return value is "hello world"
 "Hello World".toLowerCase();
 
-
+// The .includes() method is called on the string "Hello World"
+// .includes("Hello") is to see if "Hello World"
+// Because "Hello World" DOES include "Hello", the return value is `true`
 "Hello World".includes("Hello");
 
-
+// The .endsWith() method is called on the string "Hello World"
+// Because "Hello World" DOES NOT end with "Hello", our return value is `false`
 "Hello World".endsWith("Hello");
 
-
+// The .endsWith method is called on the string "Hello World"
+// Because "Hello World" DOES end with "rld", our return value is `true`
 "Hello World".endsWith("rld");
 
 
@@ -33,6 +37,17 @@
 var firstName = "Jeff";
 console.log(firstName.startsWith("J"));
 
+// The .repeat() method is called on the pet variable, which stores the string object "Louis Kitty".
+// The .repeat() method repeats the string variable as many times as we specify. In this case, that is 4. 
+// console.log() prints "Louis Kitty" 4 times to the console.
+var pet = "Louis Kitty ";
+console.log(pet.repeat(4));
+
+// The .toUpperCase() method is called on the yuck variable, which stores the string "Boogers are not for eating."
+// The .toUpperCase() method returns all of the characters in a string as uppercase. 
+// console.log() prints this sentence (in uppercase) in the console. 
+var yuck = "Boogers are not for eating.";
+console.log(yuck.toUpperCase());
 
 
 // SECTION 3: Calling methods on variables assigned to arrays.
@@ -40,3 +55,25 @@ console.log(firstName.startsWith("J"));
 // Call a different built-in JavaScript method on each of your variables. 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 // Include comments above each method call explaining the impact and return value of that method.
+
+
+// The pets array stores 4 different string values. 
+// The .pop() method removes the last element ("snake") from an array and returns that element. 
+// After applying .pop() to pets, I print console.log(pets). This essentially removed "snake" from the printed output and puts "snake" as the return value. 
+var pets = ["cat", "dog", "parrot", "snake"];
+pets.pop();
+console.log(pets);
+
+// The foods array stores 4 different string values. 
+// I made a loop to demonstrate what the .includes() method does. 
+// The .includes() method sees if each element includes a character or set of characters. In my example, we are seeing if each string contains the letter "o".
+// The .includes() method returns either a true or a false value, dependent on whether or not each element contains "o". 
+// Instead of printing true / false, I wanted the loop to print only the foods that contain the letter "o".
+var foods = ["popcorn", "cheetos", "broccoli", "nutella"] 
+
+for (var i = 0; i < foods.length; i++) {
+    if (foods[i].includes("o")) {
+        console.log(foods[i])
+    }
+}
+
